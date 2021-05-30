35da8574052a0f63bd507efa9cb0b38980737456
selpo <planet_selpo@yahoo.co.jp>
2021-05-30 17:16:20 +0900

    move optshape.cpp to Rust

--- include/lrcalc/optshape.hpp
+++ include/lrcalc/optshape.hpp
@@ -19,12 +19,6 @@ extern "C"
 
 	skew_shape optim_mult(const ivector* sh1, const ivector* sh2, int maxrows, int maxcols);
 
-	void _optim_fusion(skew_shape* ss, const ivector* sh1, const ivector* sh2, int maxrows, int maxcols);
-
-	void _optim_skew(skew_shape* ss, const ivector* outer, const ivector* inner, const ivector* content, int maxrows);
-
-	void _optim_coef(skew_shape* ss, const ivector* out, const ivector* sh1, const ivector* sh2);
-
 	skew_shape optim_fusion(const ivector* sh1, const ivector* sh2, int maxrows, int maxcols);
 
 	skew_shape optim_skew(const ivector* outer, const ivector* inner, const ivector* content, int maxrows);
